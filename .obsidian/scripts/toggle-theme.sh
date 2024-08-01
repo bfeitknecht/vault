39705dffff5402dfd5a4b9aacf5c8df32939d469
bfeitknecht <bfeitknecht@ethz.ch>
2024-08-01 09:52:58 +0200

    backup: 2024-08-01T09:52:58+02:00 from MacBook Pro (echo)
    Affected files:
    .obsidian/appearance.json
    .obsidian/graph.json
    .obsidian/plugins/obsidian-shellcommands/data.json
    .obsidian/scripts/toggle-theme.sh

--- .obsidian/scripts/toggle-theme.sh
+++ .obsidian/scripts/toggle-theme.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-# dark: "theme": "obsidian",
-# light: "theme": "moonstone",
-
 FILE_PATH="/Users/bf/home/eth/vault/.obsidian/appearance.json"
 
-sed -i '' -e '/"theme": "obsidian"/ s/"theme": "obsidian"/"theme": "moonstone"/' \
-          -e '/"theme": "moonstone"/ s/"theme": "moonstone"/"theme": "obsidian"/' "$FILE_PATH"
+CURRENT_THEME=$(jq -r '.theme' "$FILE_PATH")
 
+if [ "$CURRENT_THEME" == "moonstone" ]; then
+    open --background "obsidian://advanced-uri?vault=vault&commandid=theme%253Ause-dark"
+elif [ "$CURRENT_THEME" == "obsidian" ]; then
+    open --background "obsidian://advanced-uri?vault=vault&commandid=theme%253Ause-light"
+fi
 
-open --background "obsidian://advanced-uri?vault=vault&commandid=theme%253Ause-dark"
-
-open --background "obsidian://advanced-uri?vault=vault&commandid=theme%253Ause-light"
