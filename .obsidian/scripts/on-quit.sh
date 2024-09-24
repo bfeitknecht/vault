71b0a63eb287e1b9e671e5401b9d07608da085e6
bfeitknecht <bfeitknecht@ethz.ch>
2024-09-24 18:45:11 +0200

    feat: on quit script to save ws and commit to git
    modified:   .obsidian/scripts/on-quit.sh
    modified:   .obsidian/workspaces.json

--- .obsidian/scripts/on-quit.sh
+++ .obsidian/scripts/on-quit.sh
@@ -3,4 +3,16 @@
 # save workspace
 open --background "obsidian://advanced-uri?vault=vault&commandid=workspaces%253Asave"
 
+# change to vault root
+cd "$1" || {
+  echo "Error changing directory!"
+  exit 1
+}
+
+# stage all changes
+git add .
+
+# commit with on quit message
+t=$(date -Iseconds)
+git commit -m "backup(quit): $t on quit from MacBook Pro (echo)"
 
