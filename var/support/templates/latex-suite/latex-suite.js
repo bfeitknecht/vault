fd1e368945161ba56c864f9c0a094488a5f36f3d
bfeitknecht <bfeitknecht@ethz.ch>
2024-09-25 15:17:13 +0200

    backup: 2024-09-25T15:17:13+02:00 from MacBook Pro (echo)
    Affected files:
    var/support/templates/latex-suite/latex-suite.js

--- var/support/templates/latex-suite/latex-suite.js
+++ var/support/templates/latex-suite/latex-suite.js
@@ -8,27 +8,27 @@
   flags?: string,
 }
 
-trigger : The text that triggers this snippet.
-Triggers can also be regular expressions. See here for more info.
-replacement : The text to replace the trigger with.
-Replacements can also be JavaScript functions. See here for more info.
-options : See below.
-priority (optional): This snippet's priority. Snippets with higher priority are run first. Can be negative. Defaults to 0.
-description (optional): A description for this snippet.
-flags (optional): Flags for regex snippets.
+trigger : 					The text that triggers this snippet. Triggers can also be regular expressions.
+replacement : 				The text to replace the trigger with. Replacements can also be JavaScript functions.
+options : 					See below.
+priority (optional) :		This snippet's priority. Snippets with higher priority are run first. Can be negative. Defaults to 0.
+description (optional) :	A description for this snippet.
+flags (optional) : 			Flags for regex snippets.
 
-options:
-t : Text mode. Only run this snippet outside math
-m : Math mode. Only run this snippet inside math. Shorthand for both M and n
-M : Block math mode. Only run this snippet inside a $$ ... $$ block
-n : Inline math mode. Only run this snippet inside a $ ... $ block
-A : Auto. Expand this snippet as soon as the trigger is typed. If omitted, the Tab key must be pressed to expand the snippet
-r : Regex. The trigger will be treated as a regular expression
-v : Visual. Only run this snippet on a selection. The trigger should be a single character
-w : Word boundary. Only run this snippet when the trigger is preceded (and followed by) a word delimiter, such as ., ,, or -.
-c : Code mode. Only run this snippet inside a ``` ... ``` block
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+options :
+	t : Text mode. Only run this snippet outside math
+	m : Math mode. Only run this snippet inside math. Shorthand for both M and n
+	M : Block math mode. Only run this snippet inside a $$ ... $$ block
+	n : Inline math mode. Only run this snippet inside a $ ... $ block
+	A : Auto. Expand this snippet as soon as the trigger is typed. If omitted, the Tab key must be pressed to expand the snippet
+	r : Regex. The trigger will be treated as a regular expression
+	v : Visual. Only run this snippet on a selection. The trigger should be a single character
+	w : Word boundary. Only run this snippet when the trigger is preceded (and followed by) a word delimiter, such as ., ,, or -.
+	c : Code mode. Only run this snippet inside a ``` ... ``` block
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 Insert tabstops for the cursor to jump to by writing "$0", "$1", etc. in the replacement.
-
 */
 
 
@@ -39,9 +39,9 @@ Insert tabstops for the cursor to jump to by writing "$0", "$1", etc. in the rep
     {trigger: "beg", replacement: "\\begin{$0}\n$1\n\\end{$0}", options: "mA"},
 
     // Dashes
-    // {trigger: "--", replacement: "–", options: "tA"},
-    // {trigger: "–-", replacement: "—", options: "tA"},
-    // {trigger: "—-", replacement: "---", options: "tA"},
+    //{trigger: "--", replacement: "–", options: "tA"},
+    //{trigger: "–-", replacement: "—", options: "tA"},
+    //{trigger: "—-", replacement: "---", options: "tA"},
     
     // Greek letters
     {trigger: "@a", replacement: "\\alpha", options: "mA"},
@@ -72,7 +72,7 @@ Insert tabstops for the cursor to jump to by writing "$0", "$1", etc. in the rep
     // Text environment
     {trigger: "text", replacement: "\\text{$0}$1", options: "mA"},
     
-    // i want empty escapes
+    /* i want empty escapes */
     //{trigger: "\"", replacement: "\\text{$0}$1", options: "mA"},
 
     // Basic operations
@@ -99,9 +99,11 @@ Insert tabstops for the cursor to jump to by writing "$0", "$1", etc. in the rep
     {trigger: "trace", replacement: "\\mathrm{Tr}", options: "mA"},
 
     // More operations
-    //
-  // {trigger: "([a-zA-Z])hat", replacement: "\\hat{[[0]]}", options: "rmA"},
-    {trigger: "([a-zA-Z])bar", replacement: "\\bar{[[0]]}", options: "rmA"},
+    
+	/* i need my hat */
+    //{trigger: "([a-zA-Z])hat", replacement: "\\hat{[[0]]}", options: "rmA"},
+    
+	{trigger: "([a-zA-Z])bar", replacement: "\\bar{[[0]]}", options: "rmA"},
     {trigger: "([a-zA-Z])dot", replacement: "\\dot{[[0]]}", options: "rmA", priority: -1},
     {trigger: "([a-zA-Z])ddot", replacement: "\\ddot{[[0]]}", options: "rmA", priority: 1},
     {trigger: "([a-zA-Z])tilde", replacement: "\\tilde{[[0]]}", options: "rmA"},
@@ -113,6 +115,7 @@ Insert tabstops for the cursor to jump to by writing "$0", "$1", etc. in the rep
     {trigger: "\\\\(${GREEK})\\.,", replacement: "\\boldsymbol{\\[[0]]}", options: "rmA"},
 
     //{trigger: "hat", replacement: "\\hat{$0}$1", options: "mA"},
+
     {trigger: "bar", replacement: "\\bar{$0}$1", options: "mA"},
     {trigger: "dot", replacement: "\\dot{$0}$1", options: "mA", priority: -1},
     {trigger: "ddot", replacement: "\\ddot{$0}$1", options: "mA"},
@@ -176,8 +179,11 @@ Insert tabstops for the cursor to jump to by writing "$0", "$1", etc. in the rep
     {trigger: "sub=", replacement: "\\subseteq", options: "mA"},
     {trigger: "sup=", replacement: "\\supseteq", options: "mA"},
     {trigger: "eset", replacement: "\\varnothing", options: "mA"},
-    //{trigger: "set", replacement: "\\{ $0 \\}$1", options: "mA"},
-    {trigger: "e\\xi sts", replacement: "\\exists", options: "mA", priority: 1},
+    
+	/* set is used too often in text */
+	//{trigger: "set", replacement: "\\{ $0 \\}$1", options: "mA"},
+    
+	{trigger: "e\\xi sts", replacement: "\\exists", options: "mA", priority: 1},
 
     {trigger: "LL", replacement: "\\mathcal{L}", options: "mA"},
     {trigger: "HH", replacement: "\\mathcal{H}", options: "mA"},
